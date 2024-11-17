e95664d7afacb955852ef14446bfc0fc05ec881d
ek775 <eliotkmiec@gmail.com>
2024-11-17 13:54:19 -0600

    re-built env to address serialization incompat

--- objects/autoencoder.py
+++ objects/autoencoder.py
@@ -1,46 +1,21 @@
 import tensorflow as tf
 import keras
-from keras import layers
 
-@keras.saving.register_keras_serializable(package='SAE', name='SparseAutoEncoder')
-class SparseAutoEncoder(keras.Model):
-    def __init__(self, encoding_size:int, expansion_factor:int, **kwargs):
-        super().__init__(**kwargs)
+@keras.saving.register_keras_serializable()
+class SparseAutoEncoder(tf.keras.Model):
+    def __init__(self, encoding_size:int, expansion_factor:int, name="sparse_auto_encoder", **kwargs):
+        super().__init__(name=name, **kwargs)
         self.n_features = encoding_size * expansion_factor
-        self.input_layer = layers.InputLayer(shape=(encoding_size,))
-        self.encoder = layers.Dense(
+        self.input_layer = tf.keras.layers.InputLayer(input_shape=(encoding_size,))
+        self.encoder = tf.keras.layers.Dense(
             units=self.n_features,
+            use_bias=False,
             activation='relu',
             kernel_regularizer='l1',
             activity_regularizer='l1'
         )
-        self.decoder = layers.Dense(units=encoding_size, activation='relu')
+        self.decoder = tf.keras.layers.Dense(units=encoding_size, use_bias=False, activation='relu')
         self.last_features = None
-    
-    def get_config(self):
-        config = super().get_config().copy()
-        config.update(
-            {
-                "input_layer":self.input_layer,
-                "encoder":self.encoder,
-                "decoder":self.decoder,
-                "n_features":self.n_features,
-                "encoding_size":self.decoder.units,
-                "expansion_factor":self.n_features/self.decoder.units
-            }
-        )
-        return config
-    
-    @classmethod
-    def from_config(cls, config):
-        return cls(**config)
-    
-    def build(self, input_shape):
-        self.add_weight(
-            shape=(input_shape[-1],),
-            initializer="glorot_uniform",
-            trainable=True
-        )
 
     def call(self, x):
         x = self.encoder(x)
@@ -49,3 +24,13 @@ class SparseAutoEncoder(keras.Model):
         # decode back to original space
         x = self.decoder(x)
         return x, self.last_features
+    
+    def get_config(self):
+        config = super().get_config()
+        config.update({
+            'encoding_size': self.encoder.units,
+            'expansion_factor': self.n_features // self.encoder.units
+        })
+        return config
+    
+    # default from_config & build methods
