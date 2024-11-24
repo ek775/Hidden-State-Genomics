689dd424278b653d22f30a71e70fb7b78eca0940
ek775 <eliotkmiec@gmail.com>
2024-11-23 21:19:36 -0600

    asynchronous embedding

--- pipelines/swissprot_embedding.py
+++ pipelines/swissprot_embedding.py
@@ -1,51 +1,69 @@
-import os
+import os, sys
 from Bio import SeqIO
 import numpy as np
 from transformers import AutoTokenizer, TFEsmModel
 from csv import writer
 from tqdm import tqdm
+import asyncio
 
 
 # load swissprot data and generate embeddings
+if len(sys.argv) != 2:
+    raise Exception("This script requires a starting position (int) and the number of ESM instances this system is capable of handling (int).")
+
+restart = sys.argv[0]
+num_workers = sys.argv[1]
 
 # file locations and ids
-embed_directory = '../data/swissprot/embeddings/'
-meta_file = '../data/swissprot/sequence_metadata.csv'
+embed_directory = './data/swissprot/embeddings/'
+meta_file = './data/swissprot/sequence_metadata.csv'
 hug_face_id = "facebook/esm2_t33_650M_UR50D"
 
 # make csv file for metadata
-if os.path.exists(meta_file):
-    raise Exception('metadata file already exists')
-else:
-    os.system(f'touch {meta_file}')
-
+os.system(f'touch {meta_file}')
 os.makedirs(embed_directory, exist_ok=True)
 
 # load model
 hug_face_id = "facebook/esm2_t33_650M_UR50D"
-tokenizer = AutoTokenizer.from_pretrained(hug_face_id)
-ESM_1280 = TFEsmModel.from_pretrained(hug_face_id)
+workers = [
+    (
+        AutoTokenizer.from_pretrained(hug_face_id), 
+        TFEsmModel.from_pretrained(hug_face_id)
+     )
+     for i in range(num_workers)
+]
 
-with open('../data/swissprot/uniprot_sprot.fasta') as f:
+# core function
+async def embed(SeqIO_entry, tokenizer, esm_model):
+    sequence = str(SeqIO_entry.seq)
+    id = str(SeqIO_entry.id)
+    description = (SeqIO_entry.description)
+
+    # generate embeddings
+    input = tokenizer(sequence, return_tensors='tf')
+    output = esm_model(input)
+    # save embeddings as numpy binaries
+    temp_file = f"{embed_directory}{id}.npy"
+    with open(temp_file, "wb") as f:
+        np.save(f, np.array(output.last_hidden_state))
+    os.system(f"gcloud storage cp {temp_file} gs://ek990/swiss-prot-esm-1280/embeddings/")
+    os.remove(temp_file)
+    # append metadata to csv
+    with open(meta_file, 'a') as f:
+        csv_writer = writer(f)
+        csv_writer.writerow([id, description, sequence])
+
+# main coroutine generator
+async def main(SeqIO_entry, worker_list):
+    await asyncio.gather(
+        *[embed(SeqIO_entry, w[0], w[1]) for w in worker_list]
+    )
+
+### Main Loop ###    
+with open('./data/swissprot/uniprot_sprot.fasta') as f:
     for entry in tqdm(SeqIO.parse(f, 'fasta'), total=572214):
+        asyncio.run(main(entry, worker_list=workers))
         
-        # get metadata
-        sequence = str(entry.seq)
-        id = str(entry.id)
-        description = (entry.description)
-
-        # generate embeddings
-        input = tokenizer(sequence, return_tensors='tf')
-        output = ESM_1280(input)
-        # save embeddings as numpy binaries
-        with open(f"{embed_directory}{id}.npy", "wb") as f:
-            np.save(f, np.array(output.last_hidden_state))
-        os.system(f"gcloud storage cp {embed_directory}{id}.npy gs://ek990/swiss-prot-esm-1280/embeddings/{id}.npy")
-
-        # append metadata to csv
-        with open(meta_file, 'a') as f:
-            csv_writer = writer(f)
-            csv_writer.writerow([id, description, sequence])
 
 # upload metadata to cloud storage
 os.system(f"gcloud storage cp {meta_file} gs://ek990/swiss-prot-esm-1280/sequece_metadata.csv")
