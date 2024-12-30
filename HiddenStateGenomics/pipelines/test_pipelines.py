e61d1745f11bd427359f45761ebef78391d4e7b1
ek775 <eliotkmiec@gmail.com>
2024-12-30 13:17:20 -0600

    first draft of refseq mutation method

--- HiddenStateGenomics/pipelines/test_pipelines.py
+++ HiddenStateGenomics/pipelines/test_pipelines.py
@@ -21,36 +21,65 @@ class TestDNAVariant(unittest.TestCase):
 
         for var in tqdm(self.clin_gen["#Variation"]):
 
-            if var is not str:
-                continue
+            var = str(var)
 
             expression = self.worker.clean_hgvs(var)
-            var_obj = self.worker.parse_variant(expression)
+            var_obj = self.worker.parse_variant(expression, return_exceptions=False)
 
-            if var_obj is not None:
-                self.assertIsInstance(var_obj, SequenceVariant)
-
-            else:
+            if var_obj is None:
                 invalid_expressions += 1
+                continue
+            
+            self.assertIsInstance(var_obj, SequenceVariant)
 
-        print(f"Found {invalid_expressions} invalid expressions out of {len(self.clin_gen)}")
+        print(f"Unable to process {invalid_expressions} out of {len(self.clin_gen)}")
+        print("""This may be due to invalid or uncertain HGVS expressions. See https://hgvs-nomenclature.org/stable/recommendations/summary/ for proper syntax. \n
+              Note that the hgvs package does not currently support all HGVS expression types.""")
 
 
     def test_retrieve_refseq(self):
 
         for var in tqdm(self.clin_gen["#Variation"]):
 
-            if var is not str:
-                continue
+            var = str(var)
             
             expression = self.worker.clean_hgvs(var)
-            var_obj = self.worker.parse_variant(expression)
+            var_obj = self.worker.parse_variant(expression, return_exceptions=False)
 
-            if var_obj is not None:
+            if var_obj is None:
+                continue
+
+            else:
                 refseq = self.worker.retrieve_refseq(var_obj)
                 self.assertIsInstance(refseq, str)
-            else:
+
+    
+    def test_retrieve_variantseq(self):
+
+        bad_mapping = 0
+
+        for var in tqdm(self.clin_gen["#Variation"]):
+
+            var = str(var)
+
+            expression = self.worker.clean_hgvs(var)
+            var_obj = self.worker.parse_variant(expression, return_exceptions=False)
+            varseq = ''
+
+            if var_obj is None:
                 continue
+
+            else:
+                varseq = self.worker.retrieve_variantseq(var_obj)
+            
+            ###
+            if varseq is None:
+                bad_mapping += 1
+            
+            else:
+                self.assertIsInstance(varseq, str)
+
+        print(f"Unable to map {bad_mapping} out of {len(self.clin_gen)}")
 
 
 # making unittest run from command line
--- HiddenStateGenomics/pipelines/variantmap.py
+++ HiddenStateGenomics/pipelines/variantmap.py
@@ -17,7 +17,7 @@ class DNAVariantProcessor():
         # hgvs tools
         self.parser:Parser = Parser()
         self.assembly_mapper:AssemblyMapper = AssemblyMapper(connect(), assembly_name=assembly, alt_aln_method='splign')
-        self.seq_repo:SeqRepo = SeqRepo(seqrepo_path) # TODO: dynamically set database 
+        self.seq_repo:SeqRepo = SeqRepo(seqrepo_path) 
 
 
     def clean_hgvs(self, raw_hgvs_expression:str) -> str:
@@ -26,30 +26,39 @@ class DNAVariantProcessor():
         Processes hgvs expressions with gene name and predicted protein change annotations and removes them for machine readability.
         """
 
+        # remove gene annotations
         pattern = r"\([^)]*\)"
         clean_hgvs = re.sub(pattern, '', raw_hgvs_expression)
 
+        # resulting string
         return clean_hgvs.strip()
 
 
-    def parse_variant(self, hgvs_expression:str) -> SequenceVariant:
+    def parse_variant(self, hgvs_expression:str, return_exceptions:bool = True ) -> SequenceVariant:
 
         """
-        Uses parser to parse input hgvs_expression
+        Uses parser to parse input hgvs_expression.
+
+        If hgvs cannot parse the expression, it will return an exception.
+
+        This behavior can be changed by setting return_exceptions to False, which, will cause invalid or uncertain expressions to return None -- useful during ML training.
         """
 
         try:
-            return self.parser.parse_hgvs_variant(hgvs_expression)  
+            return self.parser.parse(hgvs_expression)  
          
         except Exception as e: 
-            print(f"Invalid or Uncertain HGVS expression: {hgvs_expression}")
-            print(e)
-            return None
+            # sometimes we want information, sometimes we just want to move on
+            if return_exceptions == True:
+                return e
+            else:
+                return None
         
 
     def standardize_expression_type(self, hgvs_ref:SequenceVariant, expression_type:str = ["g","m","c","n","r","p"]):
 
         """
+        TODO:
         Standardize hgvs types for proper mapping of refseqs and generation of variant sequences.
 
         Different hgvs types utilize different coordinate systems and may use non-contiguous indices, 
@@ -72,13 +81,39 @@ class DNAVariantProcessor():
     def retrieve_variantseq(self, hgvs_ref: SequenceVariant) -> str:
 
         """
-        Modifies obtained refseq sequence to obtain the variant sequence.
+        Modifies obtained refseq sequence to obtain the variant sequence
         """
 
         variant_start: int = hgvs_ref.posedit.pos.start.base
         variant_end: int = hgvs_ref.posedit.pos.end.base
-        ref: str = hgvs_ref.posedit.edit.ref
-        var: str = hgvs_ref.posedit.edit.alt
 
-        pass
+        # some variant types may not have a reference or variant allele (i.e. copy number variants)
+        try:
+            ref: str = hgvs_ref.posedit.edit.ref
+            assert ref is not None
+        except:
+            ref:str = ''
+
+        try:
+            var: str = hgvs_ref.posedit.edit.alt
+            assert var is not None
+        except:
+            var:str = ''
+
+        # ref and var should not both be None
+        if ref == None and var == None:
+            raise ValueError("Reference and Variant alleles cannot both be None")
+
+        # get refseq
+        refseq = self.seq_repo[f"refseq:{hgvs_ref.ac}"]
+        varseq = ''
+
+        # modify the refseq to obtain the variant sequence
+        try:
+            assert ref == refseq[variant_start:variant_end]
+            varseq = refseq[:variant_start] + var + refseq[variant_end:]
+        except:
+            varseq = None
+        
+        return varseq
 
