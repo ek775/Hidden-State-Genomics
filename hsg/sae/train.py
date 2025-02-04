b644137f359c0ac56469dae5bd2376dbfd2fc854
ek775 <eliotkmiec@gmail.com>
2025-02-04 16:24:10 -0600

    fixed spark oauth, filesystem config

--- hsg/sae/train.py
+++ hsg/sae/train.py
@@ -4,6 +4,7 @@ import torch
 import torch.nn as nn
 from hsg.sae.dictionary import AutoEncoder
 from google.cloud import storage
+import google.auth
 
 import os
 
@@ -11,20 +12,52 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+# google authentication for hadoop
+credentials, project = google.auth.default()
+
 
 ### HELPER FUNCTIONS ###
+def config_spark() -> SparkSession:
+    """
+    Configures the spark session with the necessary settings for GCS. 
+    
+    Note that spark does not support native gcloud sdk authentication, so we need to provide the credentials manually.
+    This function utilizes the python gcloud sdk to pass the appropriate credentials to the spark session for security.
+    """
+    spark = SparkSession.builder\
+        .master("local")\
+        .appName("SAE")\
+        .config("spark.jars", "https://storage.googleapis.com/hadoop-lib/gcs/gcs-connector-hadoop3-latest.jar")\
+        .getOrCreate()
+    
+    spark._jsc.hadoopConfiguration().set('fs.gs.impl', 'com.google.cloud.hadoop.fs.gcs.GoogleHadoopFileSystem')
+    spark._jsc.hadoopConfiguration().set('fs.AbstractFileSystem.gs.impl', 'com.google.cloud.hadoop.fs.gcs.GoogleHadoopFS')
+    spark._jsc.hadoopConfiguration().set("google.cloud.auth.service.account.enable", "true")
+    spark._jsc.hadoopConfiguration().set('fs.gs.project.id', project)
+    spark._jsc.hadoopConfiguration().set('fs.gs.auth.client.id', credentials.client_id)
+    spark._jsc.hadoopConfiguration().set('fs.gs.auth.client.secret', credentials.client_secret)
+    spark._jsc.hadoopConfiguration().set('fs.gs.auth.refresh.token', credentials.refresh_token)
 
-def load_data(data_path: str) -> DataFrame:
+    spark.conf.set("spark.sql.repl.eagerEval.enabled", True)
+
+    return spark
+
+
+def load_data(data_path: str, spark: SparkSession) -> DataFrame:
     """
     Loads SAE training data from a given csv file via spark.
 
     Args:
         data_path: The path to the csv file containing the training data.
+        spark: The spark session to use for loading the data.
     Returns:
         A DataFrame containing the training data.
     """
-    spark = SparkSession.builder.appName("SAE").getOrCreate()
-    data = spark.read.csv(data_path, header=True)
+    # load data
+    print(f"Loading data from {data_path}")
+    data = spark.read.csv(data_path, inferSchema=True)
+    print("=== DONE ===")
+    print(data.head())
     return data
 
 
@@ -66,6 +99,8 @@ def main(
     """
     num_layers = 0
 
+    spark_config = config_spark()
+
     if data_path.startswith("gs://"):
         storage_client = storage.Client()
         bucket = storage_client.get_bucket(data_path.split("/")[2])
@@ -77,7 +112,7 @@ def main(
 
         autoencoder = get_model(activation_dim=input_size, dict_size=feature_size)
         print(autoencoder)
-        embed_df = load_data(data_path=data_path)
+        embed_df = load_data(data_path=data_path, spark=spark_config)
         print(embed_df.head())
 
 
