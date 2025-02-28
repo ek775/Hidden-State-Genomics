894088cea8dfd35adff0e417d132187c2e044614
samuel-ob <sam.t.obrien@gmail.com>
2025-02-27 22:06:10 -0600

    Varmap pipeline in workable order

--- hsg/pipelines/variantmap.py
+++ hsg/pipelines/variantmap.py
@@ -14,7 +14,6 @@ class DNAVariantProcessor():
     """
 
     def __init__(self, assembly:str = "GRCh37", seqrepo_path:str = "./genome_databases/2024-12-20") -> None:
-        # hgvs tools
         self.parser:Parser = Parser()
         self.assembly_mapper:AssemblyMapper = AssemblyMapper(connect(), assembly_name=assembly, alt_aln_method='splign')
         self.seq_repo:SeqRepo = SeqRepo(seqrepo_path) 
@@ -57,47 +56,42 @@ class DNAVariantProcessor():
         Processes a deletion by deleting the specified sequence region.
         """
         if int(variant_start) == int(variant_end):
-            #print('same')
             ref_seq = str(self.seq_repo[f"refseq:{hgvs_ref.ac}"])[int(variant_start)-2999 : int(variant_end)+2999]
             var_seq = var_seq = ref_seq[:2998]+ref_seq[2999:]
             return(var_seq)
+
         elif int(variant_start) != int(variant_end):
             del_len = int(variant_end) - (int(variant_start)-1)
-            add_len = del_len/2
-            if isinstance(add_len, float):
-                lower = (int(add_len))
-                higher = lower +1
-                ref_seq = str(self.seq_repo[f"refseq:{hgvs_ref.ac}"])[int(variant_start)-2999 : int(variant_end)+2999]
-                print(ref_seq)
-                var_seq = ref_seq[:2998]+ref_seq[2998+del_len:]
-                print(var_seq)
-            #print(del_len)
-            #print(variant_start)
-            #print(variant_end)
-            ref_seq = self.retrieve_refseq(hgvs_ref)
-        return ref_seq
+            ref_seq = str(self.seq_repo[f"refseq:{hgvs_ref.ac}"])[int(variant_start)-2999 : int(variant_end)+2999]
+            var_seq = ref_seq[:2998]+ref_seq[2998+del_len:]
+            return(var_seq)
 
     def process_snp(self, hgvs_ref: str, variant_start: int, variant_end: int) -> str:
         """
         Processes a SNP by replacing the affected base.
         """
         ref_seq = self.retrieve_refseq(hgvs_ref)
-        #print(hgvs_ref)
-        #print(ref_seq[2998])
-        #print(len(ref_seq))
         alt_nuc = str(hgvs_ref.posedit.edit.alt)
         var_seq = ref_seq[:2998]+alt_nuc+ref_seq[2999:]
-        #print(var_seq[2998])
-        #print(len(var_seq))
         return var_seq
 
     def process_dup(self, hgvs_ref: str, variant_start: int, variant_end: int) -> str:
         """
         Processes a duplication by duplicating the specified sequence region.
         """
-        print("dup")
-        ref_seq = self.retrieve_refseq(hgvs_ref)
-        return ref_seq
+        if int(variant_start) == int(variant_end):
+            ref_seq = str(self.seq_repo[f"refseq:{hgvs_ref.ac}"])[int(variant_start)-2998 : int(variant_end)+2998]
+            var_seq = ref_seq[:2998]+ref_seq[2997]+ref_seq[2998:]
+            return ref_seq
+        elif int(variant_start) != int(variant_end):
+            dup_len = int(variant_end) - (int(variant_start)-1)
+            half_dup_len = dup_len/2
+            ref_seq = str(self.seq_repo[f"refseq:{hgvs_ref.ac}"])[int(variant_start-(2999+half_dup_len)) : int(variant_end+(2999-half_dup_len))]
+            mid_ref = int(len(ref_seq)/2)
+            dup_seq = ref_seq[mid_ref:mid_ref+dup_len]
+            var_seq = ref_seq[:mid_ref+dup_len]+dup_seq+ref_seq[mid_ref+dup_len:]
+            trimed_var_seq = self.trim_string_odd(var_seq, 5997)
+            return trimed_var_seq
 
     def retrieve_refseq(self, hgvs_ref:SequenceVariant) -> str:
 
@@ -132,3 +126,14 @@ class DNAVariantProcessor():
             raise ValueError(f"Unsupported variant type: {variant_type}")
 
         return varseq
+
+    def trim_string_odd(self, s: str, target_length: int) -> str:
+        current_length = len(s)
+    
+        if target_length >= current_length:
+            return s 
+    
+        trim_amount = (current_length - target_length) // 2
+        extra = (current_length - target_length) % 2  
+    
+        return s[trim_amount: -(trim_amount + extra)] if trim_amount > 0 else s
