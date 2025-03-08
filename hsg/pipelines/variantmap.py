c98d74909c2603f3ff090331d151699d7c5105e6
ek775 <eliotkmiec@gmail.com>
2025-03-08 13:04:52 -0600

    fleshing out docstrings

--- hsg/pipelines/variantmap.py
+++ hsg/pipelines/variantmap.py
@@ -20,15 +20,31 @@ load_dotenv()
 class DNAVariantProcessor():
 
     """
-    Class for processing hgvs variant expressions to obtain raw sequences for reference and variant alleles
+    Class for processing hgvs variant expressions to obtain raw sequences for reference and variant alleles.
+    Aggregates biocommons utilities for parsing hgvs nomenclature and retrieving sequences as strings.
+
+    Methods:
+    - clean_hgvs
+    - parse_variant
+    - genomic_sequence_projection
+    - determine_variant_type
+    - process_del
+    - process_snp
+    - process_dup
+    - retrieve_refseq
+    - retrieve_variantseq
+    - trim_string_odd
     """
 
     def __init__(self, assembly:str = "GRCh38.p14", seqrepo_path:str = os.environ["SEQREPO_PATH"]) -> None:
+        """
+        Start me up with a parser, assembly mapper, and sequence repository.
+        """
         # hgvs tools
         self.parser:Parser = Parser()
         self.assembly_mapper:AssemblyMapper = AssemblyMapper(
             connect(), 
-            normalize=False,
+            normalize=False, # found this can cause problems with some clingen variants
             assembly_name=assembly, 
             alt_aln_method='blat',
             replace_reference=True
@@ -39,7 +55,8 @@ class DNAVariantProcessor():
     def clean_hgvs(self, raw_hgvs_expression:str) -> str:
 
         """
-        Processes hgvs expressions with gene name and predicted protein change annotations and removes them for machine readability.
+        Processes hgvs expressions with gene name and predicted protein change annotations in parenthesis and removes 
+        them for machine readability.
         """
 
         # remove gene annotations
@@ -58,6 +75,23 @@ class DNAVariantProcessor():
         If hgvs cannot parse the expression, it will return an exception.
 
         This behavior can be changed by setting return_exceptions to False, which, will cause invalid or uncertain expressions to return None -- useful during ML training.
+        
+
+        Args:
+
+        - hgvs_expression: str
+
+            HGVS expression to be processed.
+
+        - return_exceptions: bool
+
+            If True, exceptions will be returned. If False, None will be returned for invalid or uncertain expressions.
+
+            
+        Returns:
+
+        - SequenceVariant object or None
+
         """
 
         try:
@@ -79,8 +113,8 @@ class DNAVariantProcessor():
         Returns a SequenceVariant object.
         1. Determine the coordinate type of the variant (g, c, n, t).
         2. Use the AssemblyMapper to project the variant to genomic coordinates.
-        3. Normalize the variant using the AssemblyMapper.
-        4. Return the projected and normalized variant.
+        3. If the variant appears to be referenced with a different assembly, AssemblyMapper attempts to convert the reference.
+        4. Return the projected variant.
         5. If the coordinate type is not supported, raise a ValueError.
         """
 
@@ -110,6 +144,19 @@ class DNAVariantProcessor():
 
         """
         Determines the type of variant based on the HGVS expression.
+
+        
+        Args:
+
+        - hgvs_expression: str
+
+            HGVS expression to be processed.
+
+            
+        Returns:
+
+        One of: ["del", "SNP", "dup", "unknown"]
+
         """
 
         if "del" in hgvs_expression:
@@ -126,6 +173,29 @@ class DNAVariantProcessor():
 
         """
         Processes a deletion by deleting the specified sequence region.
+
+        
+        Args:
+
+        - hgvs_ref: SequenceVariant
+
+            SequenceVariant object representing the target variant.
+
+        - variant_start: int
+
+            Start position of the deletion ***Note that this should be a zero-indexed genomic coordinate shifted by [-1].
+
+        - variant_end: int
+
+            End position of the deletion ***Note that this should be a zero-indexed genomic coordinate shifted by [-1].
+
+
+        Returns:
+
+        - var_seq: str
+
+            Sequence with the deletion applied.
+
         """
 
         del_len = int(variant_end) - (int(variant_start)-1)
@@ -148,6 +218,29 @@ class DNAVariantProcessor():
 
         """
         Processes a SNP by replacing the affected base.
+
+        
+        Args:
+
+        - hgvs_ref: SequenceVariant
+
+            SequenceVariant object representing the target variant.
+
+        - variant_start: int
+
+            Start position of the SNP ***Note that this should be a zero-indexed genomic coordinate shifted by [-1].
+
+        - variant_end: int
+
+            End position of the SNP ***Note that this should be a zero-indexed genomic coordinate shifted by [-1].
+
+            
+        Returns:
+
+        - var_seq: str
+
+            Sequence with the SNP applied.
+
         """
 
         ref_seq, offset = self.retrieve_refseq(hgvs_ref, return_offset=True)
@@ -155,15 +248,33 @@ class DNAVariantProcessor():
         var_seq = ref_seq[:offset]+alt_nuc+ref_seq[offset+1:] # start idx is inclusive, end idx is exclusive
         return var_seq
 
-#        ref_seq = self.retrieve_refseq(hgvs_ref)
-#        alt_nuc = str(hgvs_ref.posedit.edit.alt)
-#        var_seq = ref_seq[:2998]+alt_nuc+ref_seq[2999:]
-#        return var_seq
-
     def process_dup(self, hgvs_ref: SequenceVariant, variant_start: int, variant_end: int) -> str:
 
         """
         Processes a duplication by duplicating the specified sequence region.
+
+        
+        Args:
+
+        - hgvs_ref: SequenceVariant
+
+            SequenceVariant object representing the target variant.
+
+        - variant_start: int
+
+            Start position of the duplication ***Note that this should be a zero-indexed genomic coordinate shifted by [-1].
+
+        - variant_end: int
+
+            End position of the duplication ***Note that this should be a zero-indexed genomic coordinate shifted by [-1].
+
+            
+        Returns:
+
+        - var_seq: str
+
+            Sequence with the duplication applied.
+
         """
 
         dup_length = int(variant_end) - (int(variant_start)-1)
@@ -174,25 +285,37 @@ class DNAVariantProcessor():
 
         return var_seq
 
-#        if int(variant_start) == int(variant_end):
-#            ref_seq = str(self.seq_repo[f"refseq:{hgvs_ref.ac}"])[int(variant_start)-2998 : int(variant_end)+2998]
-#            var_seq = ref_seq[:2998]+ref_seq[2997]+ref_seq[2998:]
-#            return ref_seq
-#        elif int(variant_start) != int(variant_end):
-#            dup_len = int(variant_end) - (int(variant_start)-1)
-#            half_dup_len = dup_len/2
-#            ref_seq = str(self.seq_repo[f"refseq:{hgvs_ref.ac}"])[int(variant_start-(2999+half_dup_len)) : int(variant_end+(2999-half_dup_len))]
-#            mid_ref = int(len(ref_seq)/2)
-#            dup_seq = ref_seq[mid_ref:mid_ref+dup_len]
-#            var_seq = ref_seq[:mid_ref+dup_len]+dup_seq+ref_seq[mid_ref+dup_len:]
-#            trimed_var_seq = self.trim_string_odd(var_seq, 5997)
-#            return trimed_var_seq
-
 
     def retrieve_refseq(self, hgvs_ref:SequenceVariant, return_offset:bool = False) -> str:
 
         """
-        Retrieve reference sequence from seqrepo.
+        Retrieve reference sequence from seqrepo for a given variant as a string. 
+
+        
+        Args:
+
+        - hgvs_ref: SequenceVariant
+
+            SequenceVariant object representing the target variant.
+
+        - return_offset: bool
+
+            If True, the offset for the context window will be returned along with the sequence. This is also the index of the 
+            variant start position relative to the context window. For example, if the variant is at position 3000 in the 
+            context window, the offset will be 3000. 
+            
+            However, if the variant occurs near the end of an accession sequence, it may be shifted to avoid "index 
+            out of bounds" errors. For example, a variant at genomic coordinate 1755 has only 1754 bases to its left, thus,
+            you cannot have 3000 bases to the left of the variant. In this case, the offset will be 1755, indicating that the 
+            variant start position at string index position 1755, as opposed to referencing a genomic coordinate. 
+
+            
+        Returns:
+
+        The reference sequence as a string [~6000 bases long (tokenizer context window limit)]. 
+        
+        If return_offset is True, the offset will be returned as well.
+
         """
 
         full = str(self.seq_repo[hgvs_ref.ac])
@@ -218,16 +341,21 @@ class DNAVariantProcessor():
         else:
             return seq_ref
 
-#        variant_start: int = hgvs_ref.posedit.pos.start.base
-#        variant_end: int = hgvs_ref.posedit.pos.end.base
-#        seq_ref = str(self.seq_repo[f"refseq:{hgvs_ref.ac}"])[int(variant_start)-2999 : int(variant_end)+2998]
-    
-#        return seq_ref
 
     def retrieve_variantseq(self, hgvs_ref: SequenceVariant) -> str:
 
         """
         Modifies obtained refseq sequence to obtain the variant sequence
+
+        Args:
+        - hgvs_ref: SequenceVariant
+
+            SequenceVariant object representing the target variant.
+
+        Returns:
+        - var_seq: str
+
+            Sequence mutated per HGVS variant expression.
         """
 
         var_type = self.determine_variant_type(str(hgvs_ref))
@@ -252,6 +380,20 @@ class DNAVariantProcessor():
         Trims a sequence to a target length from both ends. 
 
         If the sequence length is odd, the extra base will be removed from the end of the sequence.
+
+        Args:
+        - s: str
+
+            Sequence to be trimmed.
+        
+        - target_length: int
+
+            Target length for the sequence.
+        
+            
+        Returns:
+
+        The trimmed sequence as a string.
         """
         
         current_length = len(s)
