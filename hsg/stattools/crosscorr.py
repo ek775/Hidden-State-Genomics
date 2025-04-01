c04b3d53e4e37770b1d6d7e20fe75c7f430c38a8
ek775 <eliotkmiec@gmail.com>
2025-04-01 11:55:56 -0500

    basic bed file construction & testing

---
+++ hsg/stattools/crosscorr.py
@@ -0,0 +1,222 @@
+import torch
+import os
+
+
+def binarize_features(features: torch.Tensor, threshold: float = 1.0) -> torch.Tensor:
+    """
+    Binarize features based on a threshold.
+
+    Args:
+        features (torch.Tensor): The input features to binarize.
+        threshold (float): The threshold for binarization.
+
+    Returns:
+        torch.Tensor: The binarized features.
+    """
+    return torch.where(features > threshold, torch.tensor(1), torch.tensor(0))
+
+
+
+def construct_alignments(features: torch.Tensor, tokens: list[str], chromosome: int, start: int) -> list[str]:
+    """
+    Construct alignments from features.
+
+    Args:
+        features (torch.Tensor): The binarized input features as a 1d array.
+        chromosome (int): The chromosome number.
+        start (int): The starting position of the features.
+
+    Returns:
+        list[str]: A list of strings representing the alignments in BED format.
+    """
+    alignments = []
+    sequence = ''
+    
+    alignment_start = None
+    alignment_end = None
+
+    for i, token in enumerate(tokens):
+        
+        active = features[i].item()
+
+        if active == 1 and alignment_start is None:
+            # Start of a new alignment
+            alignment_start = start + len(sequence)
+            sequence += token
+
+        if active == 1 and alignment_start is not None:
+            # Contiguous activations
+            sequence += token
+            continue
+
+        if active == 0 and alignment_start is not None:
+            sequence += token
+            # End of the current alignment
+            alignment_end = start + len(sequence)
+            # Add alignment to the list
+            alignments.append(f"chr{chromosome}\t{alignment_start}\t{alignment_end}")
+            # Reset for next alignment
+            alignment_start = None
+            alignment_end = None
+
+        else:
+            # No alignment found
+            sequence += token
+            continue
+
+    return alignments
+
+
+
+def features_to_bed(features: torch.Tensor, tokens: list[str], feature_id: str, chromosome: int, 
+                    start: int, end: int, filepath: str, description: str) -> None:
+    """
+    Convert features to BED format and write to file.
+    """
+
+    header =    f"browser position chr{chromosome}:{start}-{end}" + '\n' +\
+                "browser hide all" + '\n' +\
+                f"""track name="{feature_id}" description="{description}" """
+
+    alignments = construct_alignments(features, tokens, chromosome, start)
+
+    if os.path.exists(filepath):
+        with open(filepath, mode='a') as f:
+            for a in alignments:
+                f.write(a + '\n')
+
+    if not os.path.exists(filepath):
+        with open(filepath, mode='w') as f:
+            f.write(header + '\n')
+            for a in alignments:
+                f.write(a + '\n')
+
+
+
+def bed_to_array(filepath: str):
+    """
+    Reads an annotation track from a BED file and constructs a 1D array for comparison against features.
+    """
+    pass
+
+
+
+def cross_correlation(features, annotation_array):
+    pass
+
+
+
+# Testing to make sure the functions work as expected
+if __name__ == "__main__":
+
+    print("Testing cross-correlation functions...")
+
+    # example usage
+    import warnings
+    warnings.filterwarnings("ignore")
+
+    import re
+
+    import torch
+    import pandas as pd
+    from tqdm import tqdm
+
+    from hsg.pipelines.variantmap import DNAVariantProcessor
+    from hsg.stattools.features import *
+
+    from dotenv import load_dotenv
+    load_dotenv()
+
+    layer = 23
+    extractor = get_latent_model(os.environ["NT_MODEL"], layer_idx=layer, sae_path=f"/home/ek224/Downloads/hidden-state-genomics/sae/layer_{layer}.pt")
+
+    # load clingen dataset
+    df = pd.read_csv(os.environ["CLIN_GEN_CSV"], header="infer", sep="\t")
+
+    # parse the variants and get some unique refseq sequences
+    processor = DNAVariantProcessor()
+
+    refseqs = []
+    varseqs = []
+    var_objs = []
+    for row in tqdm(df["HGVS Expressions"][42:45]):
+
+        # try to pick out expressions listed in genomic coordinates
+        exp = row.split(",")
+        expression = None
+
+        for i, e in enumerate(exp):
+            if ":g." in e:
+                expression = e
+                break
+            if i == len(exp) -1:
+                expression = e
+            else:
+                continue
+
+        try:
+            # strip gene names & protein mutation tags
+            expression = processor.clean_hgvs(expression)
+            # parse variant
+            var_obj = processor.parse_variant(expression)
+            # perform liftover for various assemblies
+            var_obj = processor.genomic_sequence_projection(var_obj)
+            # get sequences
+            refseq, offset = processor.retrieve_refseq(var_obj, return_offset=True)
+            varseq = processor.retrieve_variantseq(var_obj)
+            # append to lists
+            refseqs.append((refseq, offset))
+            varseqs.append(varseq)
+            var_objs.append(var_obj)
+    
+        # return nulls if we can't parse the variant
+        except:
+            refseqs.append(None)
+            varseqs.append(None)
+            var_objs.append(None)
+
+    # drop nulls
+    for i in range(len(refseqs)):
+        if refseqs[i] is None or varseqs[i] is None or var_objs[i] is None:
+            refseqs.pop(i)
+            varseqs.pop(i)
+            var_objs.pop(i)
+            continue
+
+    features = []
+    original_embeds = []
+    token_sets = []
+
+    # get the features and embeddings
+    errors = []
+    for entry in tqdm(varseqs):
+    
+        with torch.no_grad():
+            feats, embeds, tokens, reconstructions = extractor.forward(entry, return_hidden_states=True, return_tokens=True, return_reconstructions=True)
+            features.append(feats.cpu())
+            original_embeds.append(embeds.cpu())
+            token_sets.append(tokens)
+
+            error = torch.nn.functional.mse_loss(reconstructions, embeds)
+            errors.append(error.item())
+        
+    # print the average error
+    print(f"Average Reconstruction MSE: {sum(errors)/len(errors)}")
+    
+    ############################################################################################################################################################
+    # Test functions with loaded data
+    ############################################################################################################################################################
+
+    # construct alignments
+    for i, feat in enumerate(features):
+
+        binarized_feat = binarize_features(feat)
+        
+        selected_feature = binarized_feat[:, 1096]
+        chromosome = re.findall(r'(\d{2})\.', str(var_objs[i].ac))[0]
+
+        alignments = construct_alignments(selected_feature, token_sets[i], chromosome, var_objs[i].posedit.pos.start.base-refseqs[i][1]) 
+
+        features_to_bed(features=selected_feature, tokens=token_sets[i], feature_id=f"feature_{i}", chromosome=chromosome, 
+                        start=var_objs[i].posedit.pos.start.base-refseqs[i][1], end=var_objs[i].posedit.pos.end.base+refseqs[i][1], 
+                        filepath=f"feature_{i}.bed", description="Feature description")
