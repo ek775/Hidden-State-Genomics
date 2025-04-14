3b9e9f750eefa0cba92217349080a8f56fb1e4e0
ek775 <eliotkmiec@gmail.com>
2025-04-14 16:07:14 -0500

    drafting reg element correlation script

---
+++ hsg/featureanalysis/regelementcorr.py
@@ -0,0 +1,18 @@
+from tap import tapify
+from tqdm import tqdm
+
+import torch
+
+from hsg.stattools.features import get_latent_model
+from hsg.stattools.crosscorr import construct_alignments, binarize_features, bed_to_array, features_to_bed, cross_correlation, xcorr_pearson
+
+
+# functions
+
+
+# main
+
+
+# run module
+if __name__ == "__main__":
+    pass
