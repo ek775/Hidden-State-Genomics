6327ff640892efab16db75eebc92cdee5fb53772
ek775 <eliotkmiec@gmail.com>
2025-08-02 19:55:25 -0500

    cisplatin prediction head training loop

--- hsg/cisplatinRNA/CNNhead.py
+++ hsg/cisplatinRNA/CNNhead.py
@@ -3,20 +3,34 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class CNNHead(nn.Module):
-    def __init__(self, input_dim, output_dim, dropout_rate=0.5):
+    def __init__(self, input_dim, seq_length, output_dim, dropout_rate=0.5):
         super(CNNHead, self).__init__()
-        kernel_size = max(int((input_dim)**0.25), 2) # minimum kernel size of 2
-        self.conv1 = nn.Conv1d(input_dim, 128, kernel_size=kernel_size, padding=0.5 * kernel_size)
-        self.conv2 = nn.Conv1d(128, 64, kernel_size=kernel_size, padding=0.5 * kernel_size)
-        self.fc = nn.Linear(64, output_dim)
+        kernel_size = max(int((input_dim)**0.2), 2) # minimum kernel size of 2
+        self.seq_length = seq_length
+        self.conv1 = nn.Conv1d(seq_length, input_dim//2, kernel_size=kernel_size, padding=int(0.5 * kernel_size))
+        self.conv2 = nn.Conv1d(input_dim//2, 64, kernel_size=kernel_size, padding=int(0.5 * kernel_size))
+        self.pooling1 = nn.MaxPool1d(kernel_size=kernel_size, stride=6)
         self.dropout = nn.Dropout(dropout_rate)
+        self.pooling2 = nn.MaxPool1d(kernel_size=4, stride=input_dim//self.pooling1.stride)
+        self.fc = nn.Linear(64, output_dim)
 
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.max_pool1d(x, kernel_size=2)
-        x = F.relu(self.conv2(x))
-        x = F.max_pool1d(x, kernel_size=2)
-        x = x.view(x.size(0), -1)  # Flatten the tensor
+        x = self.pad_sequence(x, max_length=self.seq_length)  # NTv2 tokenizer max length
+        x = x.squeeze(0)
+        x = nn.ReLU()(self.conv1(x))
+        x = nn.ReLU()(self.conv2(x))
+        x = nn.ReLU()(self.pooling1(x))
         x = self.dropout(x)
-        x = self.fc(x)
+        x = nn.ReLU()(self.pooling2(x))
+        x = self.fc(torch.mean(x, dim=1))
         return x
+    
+    def pad_sequence(self, x, max_length):
+        """
+        Pads the input sequence to the specified maximum length. Assumes input from NTv2-500m Human model with shape (seq_length, embedding_dim),
+        or (seq_length, feature_dim) for CNNHead.
+        """
+        if x.size(0) < max_length:
+            padding = torch.zeros((max_length - x.size(0), x.size(1)), device=x.device)
+            return torch.cat((x, padding), dim=0)
+        return x[:max_length, :]
--- hsg/cisplatinRNA/CNNtrain.py
+++ hsg/cisplatinRNA/CNNtrain.py
@@ -3,12 +3,16 @@ import torch.nn as nn
 import torch.nn.functional as F
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, classification_report
+from torch.utils.tensorboard import SummaryWriter
+import pandas as pd
 
+from hsg.sae.protocol.checkpoint import History
 from hsg.cisplatinRNA.CNNhead import CNNHead
 from hsg.featureanalysis.regelementcorr import read_bed_file, get_sequences_from_dataframe, gcloud_upload
 from hsg.stattools.features import get_latent_model
 
-from tap import tapify
+from biocommons.seqrepo import SeqRepo
+
 from dotenv import load_dotenv
 from tqdm import tqdm
 from tempfile import TemporaryFile
@@ -18,22 +22,160 @@ load_dotenv()
 
 ### Functions for data preparation and model training
 
-def prepare_data(cisplatin_positive, cisplatin_negative):
-    positive_df = read_bed_file(cisplatin_positive)
-    negative_df = read_bed_file(cisplatin_negative)
+def prepare_data(cisplatin_positive, cisplatin_negative) -> tuple[list, list, list]:
 
-    positive_sequences = get_sequences_from_dataframe(positive_df)
-    negative_sequences = get_sequences_from_dataframe(negative_df)
+    print(" --- Reading cisplatin BED files --- ")
+    seqrepo = SeqRepo(os.environ["SEQREPO_PATH"])
+    positive_df = read_bed_file(cisplatin_positive, max_columns=6)
+    print("Retrieving sequences from positive BED file...")
+    positive_sequences = get_sequences_from_dataframe(positive_df, seqrepo=seqrepo, pad_size=0)
+#    limit = len(positive_sequences) # there are a lot of negative sequences, so we limit how many we read to save time/memory
+    positive_sequences = list(set(positive_sequences))  # remove duplicates
+    print(f"Found {len(positive_sequences)} unique positive sequences.")
+    del positive_df  # free memory
 
-    data = [(seq, 1) for seq in positive_sequences] + [(seq, 0) for seq in negative_sequences]
+    negative_df = read_bed_file(cisplatin_negative, max_columns=6, limit=None)  # read all negative sequences
+    print(f"Retrieving sequences from negative BED file...")
+    negative_sequences = get_sequences_from_dataframe(negative_df, seqrepo=seqrepo, pad_size=0)
+    negative_sequences = list(set(negative_sequences))  # remove duplicates
+    print(f"Found {len(negative_sequences)} unique negative sequences.")
+    del negative_df  # free memory
+
+    data = [(seq, [0, 1]) for seq in positive_sequences] + [(seq, [1, 0]) for seq in negative_sequences]
+    del positive_sequences, negative_sequences  # free memory
     train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
     train_data, validation_data = train_test_split(train_data, test_size=0.2, random_state=42)
 
     return train_data, validation_data, test_data
 
 
-def train(upstream_model, prediction_head, train, validate, test, condition:str["embeddings", "features"], epochs=100, batch_size=32, learning_rate=0.001, output_dir=None):
-    
+def train(upstream_model, prediction_head, train, validate, condition:str, 
+          layer_idx, early_stop_patience=10, epochs=100, batch_size=32, learning_rate=0.001, output_dir:str=None):
+    """
+    Train a CNN model for RNA sequence classification.
+    """
+    if condition not in ["embeddings", "features"]:
+        raise ValueError("Condition must be either 'embeddings' or 'features'.")
+
+    print(f"Training {condition} model for layer {layer_idx}...")
+    # for early stopping
+    tracker = History(patience=early_stop_patience, layer=layer_idx)
+    log_dir = os.path.join(output_dir, f"layer_{layer_idx}", condition)
+
+    # for tensorboard logging
+    train_log_writer = SummaryWriter(log_dir=os.path.join(log_dir, f"layer_{layer_idx}")) 
+    layout = {
+        f"layer_{layer_idx}": {
+            "Accuracy": ["Multiline", ["Accuracy/Train", "Accuracy/Val"]],
+            "Loss": ["Multiline", ["Loss/Train", "Loss/Val"]],
+        },
+    }
+    train_log_writer.add_custom_scalars(layout)
+
+    # try to load prediction_head on GPU, if not enough memory, train on CPU
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"Using device: {device}")
+    try:
+        prediction_head.to(device)
+    except torch.OutOfMemoryError:
+        print("GPU memory insufficient to host both parent model and prediction_head. Attempting to train prediction_head on CPU.")
+        device = torch.device("cpu")
+        prediction_head.to(device)
+
+    optimizer = torch.optim.Adam(prediction_head.parameters(), lr=learning_rate)
+    loss_function = nn.CrossEntropyLoss()
+
+    # since there's a lot of data, fragment dataset into sets for each epoch
+    train_frag_size = len(train) // epochs + 1
+    train_set = [train[i:i + train_frag_size] for i in range(0, len(train), train_frag_size)]
+    val_frag_size = len(validate) // epochs + 1
+    validate_set = [validate[i:i + val_frag_size] for i in range(0, len(validate), val_frag_size)]
+
+    ### Main training loop ###
+
+    for epoch in range(epochs):
+
+        train = train_set[epoch % len(train_set)]
+        validate = validate_set[epoch % len(validate_set)]
+
+        # train & backprop
+        train_losses = []
+        labels = []
+        predictions = []
+        for seq, label in tqdm(train, desc=f"Training Epoch {epoch+1}/{epochs} - {condition}"):
+
+            if condition == "features":
+                seq_tensor = upstream_model(seq[0], return_hidden_states=True)[1].to(device).squeeze(0)  # remove batch dimension
+            else:  # condition == "embeddings"
+                seq_tensor = upstream_model(seq[0]).to(device).squeeze(0) # remove batch dimension
+
+            label_tensor = torch.tensor(label, dtype=torch.float).to(device)
+
+            prediction_head.train()
+            optimizer.zero_grad()
+            output = prediction_head(seq_tensor)
+            loss = loss_function(output, label_tensor)
+            loss.backward()
+            optimizer.step()
+
+            train_losses.append(loss.item())
+            predicted = torch.argmax(output).item()
+            label = torch.argmax(label_tensor).item()
+            predictions.append(predicted)
+            labels.append(label)
+
+        avg_train_loss = sum(train_losses) / len(train_losses)
+        avg_train_accuracy = accuracy_score(labels, predictions)
+        print(f"Train Loss: {avg_train_loss:.4f} | Train Accuracy: {avg_train_accuracy:.4f}")
+
+        # validation
+        with torch.no_grad():
+            prediction_head.eval()
+            val_losses = []
+            labels = []
+            predictions = []
+
+            for seq, label in tqdm(validate, desc=f"Validating Epoch {epoch+1}/{epochs} - {condition}"):
+
+                if condition == "features":
+                    seq_tensor = upstream_model(seq[0], return_hidden_states=True)[1].to(device).squeeze(0)  # remove batch dimension
+                else:  # condition == "embeddings"
+                    seq_tensor = upstream_model(seq[0]).to(device).squeeze(0) # remove batch dimension
+
+                label_tensor = torch.tensor(label, dtype=torch.float).to(device)
+
+                output = prediction_head(seq_tensor)
+                loss = loss_function(output, label_tensor)
+                val_losses.append(loss.item())
+
+                predicted = torch.argmax(output).item()
+                label = torch.argmax(label_tensor).item()
+                predictions.append(predicted)
+                labels.append(label)
+
+            avg_val_loss = sum(val_losses) / len(val_losses)
+            avg_val_accuracy = accuracy_score(labels, predictions)
+            print(f"Validation Loss: {avg_val_loss:.4f} | Validation Accuracy: {avg_val_accuracy:.4f}")
+
+
+        # checkpoint logic
+        tracker.update(prediction_head, avg_train_accuracy, avg_val_accuracy, epoch)
+        if tracker.early_stop:
+            print(f"Early stopping at epoch {epoch}.")
+            break
+        # log to tensorboard
+        train_log_writer.add_scalar(f"Loss/Train", avg_train_loss, epoch)
+        train_log_writer.add_scalar(f"Loss/Val", avg_val_loss, epoch)
+        train_log_writer.add_scalar(f"Accuracy/Train", avg_train_accuracy, epoch)
+        train_log_writer.add_scalar(f"Accuracy/Val", avg_val_accuracy, epoch)
+        train_log_writer.flush()
+
+        # memory cleanup fail-safe
+        if torch.cuda.is_available():
+            torch.cuda.empty_cache()
+
+
+    ### Save Results ###
 
     print("Saving results...")
 
@@ -59,14 +201,79 @@ def train(upstream_model, prediction_head, train, validate, test, condition:str[
     print("=== Done ===")
     print(f"Results saved to {output_dir}")
 
+    return prediction_head
 
-def evaluate(model, test_data):
-    pass
+
+def evaluate(upstream_model, prediction_head, test_data, condition:str, output_dir:str=None):
+    print("Evaluating model...")
+    with torch.no_grad():
+        prediction_head.eval()
+        device = prediction_head.fc.weight.device
+        test_losses = []
+        labels = []
+        predictions = []
+        results = []
+
+        for seq, label in tqdm(test_data, desc="Evaluating"):
+            
+            if condition == "features":
+                seq_tensor = upstream_model(seq[0], return_hidden_states=True)[1].to(device).squeeze(0)  # remove batch dimension
+            else:  # condition == "embeddings"
+                seq_tensor = upstream_model(seq[0]).to(device).squeeze(0) # remove batch dimension
+
+            label_tensor = torch.tensor(label, dtype=torch.float).to(device)
+
+            output = prediction_head(seq_tensor)
+            loss = nn.CrossEntropyLoss()(output, label_tensor)
+            test_losses.append(loss.item())
+
+            predicted = torch.argmax(output).item()
+            label = torch.argmax(label_tensor).item()
+            predictions.append(predicted)
+            labels.append(label)
+
+            results.append({
+                "sequence": seq[0],
+                "predicted": predicted,
+                "actual": label,
+                "loss": loss.item(),
+            })
+
+        accuracy = accuracy_score(labels, predictions)
+
+        avg_test_loss = sum(test_losses) / len(test_losses)
+        print(f"Average Test Loss: {avg_test_loss:.4f}")
+        print(f"Average Test Accuracy: {accuracy:.4f}")
+        print("Classification Report:")
+        print(classification_report([r['actual'] for r in results], [r['predicted'] for r in results]))
+        df = pd.DataFrame(results)
+
+    # upload to GCS
+    if output_dir.startswith("gs://"):
+        with TemporaryFile() as temp_file:
+            df.to_csv(temp_file, index=True)
+            temp_file.seek(0)
+            gcloud_upload(
+                temp_file, 
+                bucket_name=output_dir.split("/")[2], 
+                destination_blob_name='/'.join(output_dir.split("/")[3:])                      
+            )
+
+    else:
+        # save to local directory
+        if os.path.exists(output_dir):
+            df.to_csv(temp_file, index=True)
+        else:
+            os.makedirs(output_dir, exist_ok=True)
+            df.to_csv(temp_file, index=True)
+
+    print("=== Done ===")
+    print(f"Results saved to {output_dir}")
 
 
 ### Compose everything into a main function
 
-def main(cisplatin_positive, cisplatin_negative, layer_idx, exp_factor=8, epochs=100, batch_size=32, learning_rate=0.001, sae_dir=None, output_path=None):
+def main(cisplatin_positive, cisplatin_negative, layer_idx=23, exp_factor=8, early_stop_patience=10, epochs=100, batch_size=32, learning_rate=0.001, sae_dir=None, output_path=None):
     """
     Conduct training and evaluation of a CNN model for RNA sequence classification.
     """
@@ -83,7 +290,70 @@ def main(cisplatin_positive, cisplatin_negative, layer_idx, exp_factor=8, epochs
     train_data, validation_data, test_data = prepare_data(cisplatin_positive, cisplatin_negative)
     # Initialize models
     upstream_model = get_latent_model(os.environ["NT_MODEL"], layer_idx, sae_path=sae_path)
-    prediction_head = CNNHead(...)
+    feature_head = CNNHead(input_dim=upstream_model.sae.dict_size, seq_length=1000, output_dim=2)
+    embedding_head = CNNHead(input_dim=upstream_model.parent_model.esm.encoder.layer[layer_idx].output.dense.out_features, seq_length=1000, output_dim=2)
 
-    # Train model
-    train(upstream_model, prediction_head, train_data, validation_data, test_data)
+    # Train embedding model
+    embedding_head = train(
+        upstream_model, 
+        embedding_head, 
+        train_data, 
+        validation_data, 
+        condition="embeddings", 
+        layer_idx=layer_idx,
+        early_stop_patience=early_stop_patience,
+        epochs=epochs, 
+        batch_size=batch_size, 
+        learning_rate=learning_rate, 
+        output_dir=output_path
+    )
+
+    evaluate(upstream_model=upstream_model, embedding_head=embedding_head, test_data=test_data, condition="embeddings", output_dir=output_path)
+
+    # Train feature model
+    feature_head = train(
+        upstream_model, 
+        feature_head, 
+        train_data, 
+        validation_data, 
+        condition="features", 
+        layer_idx=layer_idx,
+        early_stop_patience=early_stop_patience,
+        epochs=epochs, 
+        batch_size=batch_size, 
+        learning_rate=learning_rate, 
+        output_dir=output_path
+    )
+
+    evaluate(upstream_model=upstream_model, feature_head=feature_head, test_data=test_data, condition="features", output_dir=output_path)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Train and evaluate CNN models for RNA sequence classification.")
+    parser.add_argument("--cisplatin_positive", type=str, required=True, help="Path to the positive cisplatin BED file.")
+    parser.add_argument("--cisplatin_negative", type=str, required=True, help="Path to the negative cisplatin BED file.")
+    parser.add_argument("--layer_idx", type=int, required=False, default=23, help="Layer index for the model.")
+    parser.add_argument("--exp_factor", type=int, default=8, help="Expansion factor for the model.")
+    parser.add_argument("--early_stop_patience", type=int, default=10, help="Patience for early stopping.")
+    parser.add_argument("--epochs", type=int, default=100, help="Number of training epochs.")
+    parser.add_argument("--batch_size", type=int, default=32, help="Batch size for training.")
+    parser.add_argument("--learning_rate", type=float, default=0.001, help="Learning rate for the optimizer.")
+    parser.add_argument("--sae_dir", type=str, default=None, help="Directory containing SAE models.")
+    parser.add_argument("--output_path", type=str, default=None, help="Output path for saving results.")
+
+    args = parser.parse_args()
+
+    main(
+        args.cisplatin_positive,
+        args.cisplatin_negative,
+        args.layer_idx,
+        exp_factor=args.exp_factor,
+        early_stop_patience=args.early_stop_patience,
+        epochs=args.epochs,
+        batch_size=args.batch_size,
+        learning_rate=args.learning_rate,
+        sae_dir=args.sae_dir,
+        output_path=args.output_path
+    )
